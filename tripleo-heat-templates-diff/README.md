ae38c43efcf43a146130b55873cc367c6f1196b0
psairam <peesapatisairam@gmail.com>
2019-02-18 01:21:12 -0500

    Adding apply patch for ffwd upgrade

--- tripleo-heat-templates-diff/README.md
+++ tripleo-heat-templates-diff/README.md
@@ -13,5 +13,5 @@ Run either `sudo ./apply_patch.py` or `sudo python apply_patch.py` to patch the 
 
 Verify that all the changes are applied. Ensure that there are no "Hunk #1 FAILED" errors. There might be "hunk ignored" warnings (based on running version) which can be ignored.   
 
-Multiple versions are supported by the script: openstack-tripleo-heat-templates-8.0.2-4.el7ost.noarch, openstack-tripleo-heat-templates-8.0.2-43.el7ost.noarch, openstack-tripleo-heat-templates-8.0.4-20.el7ost.noarch and openstack-tripleo-heat-templates-8.0.7-4.el7ost.noarch
+Multiple versions are supported by the script: openstack-tripleo-heat-templates-8.0.2-4.el7ost.noarch, openstack-tripleo-heat-templates-8.0.2-43.el7ost.noarch, openstack-tripleo-heat-templates-8.0.4-20.el7ost.noarch, openstack-tripleo-heat-templates-8.0.7-4.el7ost.noarch and openstack-tripleo-heat-templates-8.0.7-21.el7ost.noarch.
 
--- tripleo-heat-templates-diff/apply_patch.py
+++ tripleo-heat-templates-diff/apply_patch.py
@@ -21,7 +21,7 @@
 # Since different openstack-tripleo-heat-templates can require different
 # diff patches to be applied, this functionality takes care of hiding
 # those details while applying the right patch. Currently all versions
-# upto version openstack-tripleo-heat-templates-8.0.7-4 are handled.
+# upto version openstack-tripleo-heat-templates-8.0.7-21 are handled.
 # Later versions are not supported at this time.
 #
 # Usage: sudo ./apply_patch.py or sudo python apply_patch.py
@@ -36,9 +36,11 @@ from rpmUtils.miscutils import stringToVersion
 VERSION_1_CHECK = "openstack-tripleo-heat-templates-8.0.2-4.el7ost.noarch"
 VERSION_2_CHECK = "openstack-tripleo-heat-templates-8.0.2-43.el7ost.noarch"
 VERSION_3_CHECK = "openstack-tripleo-heat-templates-8.0.7-4.el7ost.noarch"
+VERSION_4_CHECK = "openstack-tripleo-heat-templates-8.0.7-21.el7ost.noarch"
 PRE_VERSION_1_DIFF = "diff_OSPD13_8.0.2-4"
 VERSION_2_DIFF = "diff_OSPD13_8.0.2-43"
 VERSION_3_DIFF = "diff_OSPD13_8.0.7-4"
+VERSION_4_DIFF = "diff_OSPD13_8.0.7-21"
 
 
 def version_compare((e1, v1, r1), (e2, v2, r2)):
@@ -74,6 +76,7 @@ def main():
         (e1, v1, r1) = stringToVersion(VERSION_1_CHECK)
         (e2, v2, r2) = stringToVersion(VERSION_2_CHECK)
         (e3, v3, r3) = stringToVersion(VERSION_3_CHECK)
+        (e4, v4, r4) = stringToVersion(VERSION_4_CHECK)
 
         args = "patch -p0 -N -d /usr/share"
 
@@ -81,6 +84,7 @@ def main():
         version_1_rc = version_compare((e0, v0, r0), (e1, v1, r1))
         version_2_rc = version_compare((e0, v0, r0), (e2, v2, r2))
         version_3_rc = version_compare((e0, v0, r0), (e3, v3, r3))
+        version_4_rc = version_compare((e0, v0, r0), (e4, v4, r4))
 
         if version_1_rc <= 0:
             args = args + " < " + file_exists(PRE_VERSION_1_DIFF)
@@ -91,7 +95,10 @@ def main():
         elif version_2_rc > 0 and version_3_rc <= 0:
             args = args + " < " + file_exists(VERSION_3_DIFF)
 
-        elif version_3_rc > 0:
+        elif version_3_rc > 0 and version_4_rc <= 0:
+            args = args + " < " + file_exists(VERSION_4_DIFF)
+
+        elif version_4_rc > 0:
             print "Not supported for %s" % version
             sys.exit(1)
 
