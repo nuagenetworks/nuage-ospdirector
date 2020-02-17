380be676a87bfdfec88af68a20f6560dff71b6a0
speesapa <sai_ram.peesapati@nokia.com>
2020-02-17 12:07:30 -0500

    Adding missing Config to create_all_roles.sh
    Change-Id: Ic6c9b464484a8f69f786a806b610c13594c6a3c3

--- nuage-tripleo-heat-templates/scripts/create_roles/create_all_roles.sh
+++ nuage-tripleo-heat-templates/scripts/create_roles/create_all_roles.sh
@@ -19,10 +19,10 @@ source /home/stack/stackrc
 # Create ComputeAvrs role
 echo "creating ComputeAvrs Role"
 mkdir -p ../../roles
-sudo openstack overcloud roles generate --roles-path ../../roles -o ../../roles/ComputeAvrs.yaml Compute
-sudo sed -i -e 's/ Compute/ ComputeAvrs/g' ../../roles/ComputeAvrs.yaml
-sudo sed -i -e "s/HostnameFormatDefault: '%stackname%-compute-%index%'/HostnameFormatDefault: '%stackname%-computeavrs-%index%'/g" ../../roles/ComputeAvrs.yaml
-sudo sed -i -e 's/- OS::TripleO::Services::NovaCompute/- OS::TripleO::Services::NovaComputeAvrs/g'   ../../roles/ComputeAvrs.yaml
+openstack overcloud roles generate --roles-path ../../roles -o ../../roles/ComputeAvrs.yaml Compute
+sed -i -e 's/ Compute/ ComputeAvrs/g' ../../roles/ComputeAvrs.yaml
+sed -i -e "s/HostnameFormatDefault: '%stackname%-compute-%index%'/HostnameFormatDefault: '%stackname%-computeavrs-%index%'/g" ../../roles/ComputeAvrs.yaml
+sed -i -e 's/- OS::TripleO::Services::NovaCompute/- OS::TripleO::Services::NovaComputeAvrs/g'   ../../roles/ComputeAvrs.yaml
 
 FILE=../../roles/ComputeAvrs.yaml
 if [ -f "$FILE" ]; then
@@ -41,10 +41,10 @@ echo "creating ${roles[*]} Role"
 
 mkdir -p ../../roles
 for role in "${roles[@]}"; do
-    sudo openstack overcloud roles generate --roles-path ../../roles -o ../../roles/${role}.yaml Compute
-    sudo sed -i -e "s/ Compute/ ${role}/g" ../../roles/${role}.yaml
-    sudo sed -i -e "s/HostnameFormatDefault: '%stackname%-compute-%index%'/HostnameFormatDefault: '%stackname%-${role,,}-%index%'/g" ../../roles/${role}.yaml
-    sudo sed -i -e "s/- OS::TripleO::Services::NovaCompute/- OS::TripleO::Services::NovaComputeAvrs/g"   ../../roles/${role}.yaml
+    openstack overcloud roles generate --roles-path ../../roles -o ../../roles/${role}.yaml Compute
+    sed -i -e "s/ Compute/ ${role}/g" ../../roles/${role}.yaml
+    sed -i -e "s/HostnameFormatDefault: '%stackname%-compute-%index%'/HostnameFormatDefault: '%stackname%-${role,,}-%index%'/g" ../../roles/${role}.yaml
+    sed -i -e "s/- OS::TripleO::Services::NovaCompute/- OS::TripleO::Services::NovaComputeAvrs/g"   ../../roles/${role}.yaml
     FILE=../../roles/${role}.yaml
     if [ -f "$FILE" ]; then
         echo "$FILE has been created"
@@ -60,12 +60,13 @@ echo "Complete!! Created  ${roles[*]} Roles files "
 # Create ComputeOvrs role
 echo "creating ComputeOvrs Role"
 mkdir -p ../../roles
-sudo openstack overcloud roles generate --roles-path ../../roles -o ../../roles/ComputeOvrs.yaml ComputeSriov
-sudo sed -i -e 's/ ComputeSriov/ ComputeOvrs/g' ../../roles/ComputeOvrs.yaml
-sudo sed -i -e 's/ Compute SR-IOV/ ComputeOvrs/g' ../../roles/ComputeOvrs.yaml
-sudo sed -i -e "s/HostnameFormatDefault: '%stackname%-computesriov-%index%'/HostnameFormatDefault: '%stackname%-computeovrs-%index%'/g" ../../roles/ComputeOvrs.yaml
-sudo sed -i -e '/- OS::TripleO::Services::NeutronSriovHostConfig/d'   ../../roles/ComputeOvrs.yaml
-sudo sed -i -e '/- OS::TripleO::Services::NeutronSriovAgent/d'   ../../roles/ComputeOvrs.yaml
+openstack overcloud roles generate --roles-path ../../roles -o ../../roles/ComputeOvrs.yaml ComputeSriov
+sed -i -e 's/ ComputeSriov/ ComputeOvrs/g' ../../roles/ComputeOvrs.yaml
+sed -i -e 's/ Compute SR-IOV/ ComputeOvrs/g' ../../roles/ComputeOvrs.yaml
+sed -i -e "s/HostnameFormatDefault: '%stackname%-computesriov-%index%'/HostnameFormatDefault: '%stackname%-computeovrs-%index%'/g" ../../roles/ComputeOvrs.yaml
+sed -i -e '/- OS::TripleO::Services::NeutronSriovHostConfig/d'   ../../roles/ComputeOvrs.yaml
+sed -i -e '/- OS::TripleO::Services::NeutronSriovAgent/d'   ../../roles/ComputeOvrs.yaml
+sed -i -e '/- OS::TripleO::Services::Ntp/a \    - OS::TripleO::Services::NuageComputeOvrs'    ../../roles/ComputeOvrs.yaml
 
 FILE=../../roles/ComputeOvrs.yaml
 if [ -f "$FILE" ]; then
